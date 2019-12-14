c7d144c70ed1d6ecd83a5b7449c3093c761c5c62
AndreiAblets <ablets.a.v@gmail.com>
2019-12-15 00:00:52 +0200

    comment

---
+++ Ablets/Laba5/JAVA_laba5/ex6.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ex6 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nType here your line:");
+
+        String word = input.nextLine();
+        String[] letters = word.split("");
+        String r_word = "";
+
+        for (int i = letters.length-1; i>=0; i--)
+            r_word += letters[i];
+
+        System.out.println("\nHere is your reversed line:");
+        System.out.println(r_word);
+    }
+
+}
